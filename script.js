d6ff5e7596336e24e1e9bd7c368ca9475760ef27
ryzecous <r3j9c89@gmail.com>
2021-01-26 13:39:16 -0500

    commit files

---
+++ script.js
@@ -0,0 +1,25 @@
+// "As a manager I want to generate a webpage that displays my team's basic info so that I have quick access to emails and GitHub profiles"
+
+// Use the [Inquirer npm package](https://github.com/SBoudrias/Inquirer.js/) to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
+
+// The dependencies are, [jest](https://jestjs.io/) for running the provided tests, and [inquirer](https://www.npmjs.com/package/inquirer) for collecting input from the user.
+
+// It is recommended that you follow this workflow:
+// 1. Run tests
+// 2. Create or update classes to pass a single test case
+// 3. Repeat
+
+
+// It is recommended that you start with a directory structure that looks like this:
+// ```
+// lib/           // classes and helper code
+// output/        // rendered output
+// templates/     // HTML template(s)
+// test/          // jest tests
+//   Employee.test.js
+//   Engineer.test.js
+//   Intern.test.js
+//   Manager.test.js
+// app.js         // Runs the application
+// ```
+
